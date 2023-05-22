4140dc5b673db2c612c5e6ec4033a67766c8559b
Joenne Spreuwers <spreuwersjoenne@gmail.com>
2023-05-22 22:29:16 +0200

    💄 JS for meta title

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,44 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import CourseView from '../views/CourseView.vue'
+import courses from '../assets/courses.json'
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+  },
+  {
+    path: '/courses/:courseId',
+    name: 'course',
+    component: CourseView
+  }
+]
 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/courses/:courseId',
-      name: 'course',
-      component: CourseView
-    },
-  ]
+  routes
 })
+
+
+const getTitle = (courseId) => {
+  const title = courses[courseId].courseTitle
+  return title
+}
+
+router.beforeEach((to, from, next) => {
+  const courseId = to.params.courseId
+  if (courseId) {
+    const title = getTitle(courseId)
+    document.title = 'corKEYS | ' + title
+  } else {
+    document.title = 'corKEYS'
+  }
+  next()
+})
+
 
 export default router
