e64619a2ca6bb83e7726e56eadc14a4f42d6f628
Joenne Spreuwers <spreuwersjoenne@gmail.com>
2023-05-22 23:21:40 +0200

    💄 Some simple styling

--- src/assets/components/courseButton.vue
+++ src/assets/components/courseButton.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <router-link
-      class="bg-slate-600 m-4 rounded px-4 py-2 hover:bg-slate-300 duration-500"
+      class="border m-4 rounded px-4 py-2 hover:bg-white hover:text-black duration-500"
       v-for="(course, key) in filteredCourses"
       :to="'/courses/' + key"
     >
@@ -27,7 +27,6 @@ export default {
   },
   computed: {
     filteredCourses() {
-      // Filter the courses based on courseClass
       return Object.entries(this.courses).reduce((filtered, [key, course]) => {
         if (course.courseClass === this.courseClassFilter) {
           filtered[key] = course
