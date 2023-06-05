644a1b78e979a97a57fc0524f2a307d5bbd9a192
Joenne Spreuwers <spreuwersjoenne@gmail.com>
2023-06-05 14:15:32 +0200

    💄 Moved button

--- src/assets/components/homeButton.vue
+++ src/assets/components/homeButton.vue
@@ -1,7 +1,7 @@
 <template>
   <router-link
     to="/"
-    class="absolute bg-slate-600 rounded-md px-4 py-2 flex items-center justify-center opacity-70 hover:bg-slate-300"
+    class="fixed left-0 bottom-0 ml-2 mb-2 shadow-md rounded-md px-4 py-2 flex items-center justify-center bg-fuchsia-500 hover:bg-white hover:text-black duration-300"
     >HOME</router-link
   >
 </template>
